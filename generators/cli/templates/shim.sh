c20cc49135e92f2d0b1b40869ed9b96b6cc6d1b1
Tobias Kiertscher <dev@mastersign.de>
2019-02-16 11:43:16 +0100

    fixed pythonpath in bash script

--- generators/cli/templates/shim.sh
+++ generators/cli/templates/shim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 APP_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-PYTHONPATH="${APP_ROOT}:${PYTHONPATH}"
+export PYTHONPATH="${APP_ROOT}:${PYTHONPATH}"
 exec python3 -m <%= rootPackage %>.cli "$@"
