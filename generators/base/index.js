988897cbb1fe0bdead85b11bdc69c7bc1b33b46a
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2019-02-28 13:37:52 +0000

    improved automation scripts

--- generators/base/index.js
+++ generators/base/index.js
@@ -138,14 +138,9 @@ module.exports = class extends Generator {
     );
 
     this.cpTpl('travis.yml', '.travis.yml');
-    this.cpTpl('auto_pipenv-restore.cmd', 'auto/pipenv-restore.cmd');
-    this.cpTpl('auto_command-test.cmd', 'auto/command-test.cmd');
-    this.cpTpl('auto_test.cmd', 'auto/test.cmd');
+    this.cp('auto', 'auto');
 
     this.cpTpl('doc_index.rst', 'doc/source/index.rst');
     this.cpTpl('doc_conf.py', 'doc/source/conf.py');
-    this.cpTpl('auto_command-doc-build.cmd', 'auto/command-doc-build.cmd');
-    this.cpTpl('auto_doc-build.cmd', 'auto/doc-build.cmd');
-    this.cpTpl('auto_doc-commit-and-push.cmd', 'auto/doc-commit-and-push.cmd');
   }
 };
